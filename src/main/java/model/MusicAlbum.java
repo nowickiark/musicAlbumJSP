56db232ca771b1b404516b11d99e3e14f1609b9a
nowickiark@outlook.com <amrentos103>
2019-08-03 11:53:47 +0200

    Begining of a Movie Album Program

---
+++ src/main/java/model/MusicAlbum.java
@@ -0,0 +1,43 @@
+package model;
+
+public class MusicAlbum {
+
+    private String genre;
+    private String title;
+    private int year;
+    private String artist;
+
+    public MusicAlbum(){};
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
